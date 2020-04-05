f69964dbd591fc77677b3c80d1504ffe2f43f465
Shashwat Garg <gargshashwat007@gmail.com>
2020-04-06 03:54:38 +0530

    High Scores Added

--- Assets/Scripts/HighscoreManager.cs
+++ Assets/Scripts/HighscoreManager.cs
@@ -1,18 +1,61 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class HighscoreManager : MonoBehaviour
 {
-    //GameManager gamemanager;
-    void Start()
-    {
-        //gamemanager = GameObject.Find("Game_Manager").GetComponent<GameManager>();
-    }
+    [SerializeField]
+    TMP_Text[] highscores = new TMP_Text[2];
 
-    // Update is called once per frame
-    void Update()
+    int newscore, oldscore, activeprofile;
+    string newname, oldname;
+
+   public void AddScore(int score, int gamemode)
     {
-        
+        string scorekey = "HScore" + gamemode;
+        string namekey = "HScore_Name" + gamemode;
+        newscore = score;
+        Debug.Log(newscore);
+        activeprofile = PlayerPrefs.GetInt("Active_Profile");
+        Debug.Log(activeprofile);
+        newname = PlayerPrefs.GetString("Profile" + activeprofile);
+        Debug.Log(newname);
+        for (int i = 0; i < 5; i++)
+        {
+            if (PlayerPrefs.HasKey(scorekey + i))
+            {
+                Debug.Log("Has Key " + i);
+                if (PlayerPrefs.GetInt(scorekey + i) < newscore)
+                {
+                    Debug.Log("Score At Pos " + i);
+                    oldscore = PlayerPrefs.GetInt(scorekey + i);
+                    oldname = PlayerPrefs.GetString(namekey + i);
+                    PlayerPrefs.SetInt(scorekey + i, newscore);
+                    PlayerPrefs.SetString(namekey + i, newname);
+                    newname = oldname;
+                    newscore = oldscore;
+                }
+            }
+            else
+            {
+                Debug.Log("Score Added" + i);
+                PlayerPrefs.SetInt(scorekey + i, newscore);
+                PlayerPrefs.SetString(namekey + i, newname);
+                newscore = 0;
+                newname = "";
+            }
+        }
+    }
+    public void ShowScore(int gamemode)
+    {
+        string scorekey = "HScore" + gamemode;
+        string namekey = "HScore_Name" + gamemode;
+        highscores[gamemode].text = "";
+        for (int i = 0; i < 5; i++)
+        {
+            highscores[gamemode].text += "\t" + (i + 1) + ". " + PlayerPrefs.GetInt(scorekey + i) + " - ";
+            highscores[gamemode].text += "\t" + PlayerPrefs.GetString(namekey + i) + " \n";
+        }
     }
 }
